266ab460c808678ecc3cd03127a5bcc9fd4b6441
lvyazhou <lvyazhou@poweroak.net>
2026-01-12 17:53:29 +0800

    补提

--- 16旋转数组.py
+++ 16旋转数组.py
@@ -3,5 +3,8 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        for i in range(k):
-            nums.insert(0,nums.pop())
+        # for i in range(k):
+        #     nums.insert(0,nums.pop())
+
+        if k := (k%len(nums)):
+            nums[:] = nums[-k:] + nums[:-k]
